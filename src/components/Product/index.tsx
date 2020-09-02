f33e31dfa4751025f5e0cfd8cfb89e36777a0506
gislainejessica <gislainehjessica@gmail.com>
2020-09-01 22:41:40 -0300

    Product layout index

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,13 +1,72 @@
-import React from 'react';
+import React from "react";
+import TshirtImage from "../../assets/tshirt.png";
 
-import { Container } from './styles';
+import {
+  Container,
+  Row,
+  Panel,
+  Column,
+  Gallery,
+  Section,
+  Description,
+} from "./styles";
 
 const Product: React.FC = () => {
   return (
     <Container>
-      <h1>Product</h1>
+      <Row>
+        <a href="#">Compartilhar</a>
+        <a href="#">Vender um igual</a>
+      </Row>
+      <Panel>
+        <Column>
+          <img src={TshirtImage} alt="Camisa Branca" className="Tshirt" />
+          <Gallery />
+
+          <Info />
+        </Column>
+        {/* <ProductAction />
+          <SellerInfo /> */}
+
+        <WarrantySection />
+        <WarrantySection />
+        <WarrantySection />
+        <Column></Column>
+      </Panel>
     </Container>
   );
 };
+
+const WarrantySection = () => (
+  <Section>
+    <h4> Garantia </h4>
+
+    <div>
+      <span>
+        <p className="title"> Garantia certificada </p>
+        <p className="descriptiob">
+          Receba seu produto ou devolvemos seu dinheiro de volta
+        </p>
+      </span>
+      <span>
+        <p className="title"> Garantia Do Vendedor</p>
+        <p className="descriptiob">Não Garantimos nada</p>
+      </span>
+    </div>
+  </Section>
+);
+
+const Info = () => (
+  <Description>
+    <h2> Descrição </h2>
+    <p>
+      A selecção de uma cadeira adequada é muito importante para prevenir
+      futuras lesões. Com esta cadeira Trevalla, terá a comodidade e bem-estar
+      que precissa em teu día. Alem disso é ideal para ser posta em qualquer
+      parte de sua casa. Seu desenho permite que adoptes no entorno que queiras
+      dando um toque mais moderno e colorido a teus espaços.
+    </p>
+  </Description>
+);
 
 export default Product;
