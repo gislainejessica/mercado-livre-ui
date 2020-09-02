ae0148d02280f6d226b990c361734d95bd719783
Support <support@leanonsystems.com>
2020-09-01 22:09:41 -0300

    config template init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "./components/Layout";
+import GlobalStyle from "./styles/GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Layout />
+    </>
+  );
+}
+
+export default App;
